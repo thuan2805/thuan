db6a495603afd9916559d1e6ccef0fe6ec7a8b38
thuận nguyễn <thuan1232014@gmail.com>
2020-09-21 13:38:09 +0700

    first commit

---
+++ js/listsv.js
@@ -0,0 +1,12 @@
+var listsv= [
+    {
+     username:"thuannd@gmail.com",
+     password:"thuan123456",
+     fullname:"Nguyễn Đức Thuận",
+     email:"thuan1232014@gmail.com",
+     gender:"true",
+     birthday:"20-09-2000",
+     schoolfee:"6.000.000",
+     marks:"9",
+ },
+]
